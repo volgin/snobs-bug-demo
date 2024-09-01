d3d56bc8ba744e384d1b8de1f2eff65a26825ada
andreivolgin <andrei@rebelation.com>
2024-09-01 15:10:49 -0400

    Removed unnecessary lines.

--- src/main/java/com/kingfishertom/snobs/repository/GenreRepository.java
+++ src/main/java/com/kingfishertom/snobs/repository/GenreRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import com.kingfishertom.snobs.model.Genre;
 
 import io.micronaut.core.annotation.NonNull;
-import io.micronaut.data.annotation.Join;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
@@ -16,12 +15,10 @@ public interface GenreRepository extends CrudRepository<Genre, Long> {
 
 	@Override
 	@NonNull
-	@Join(value = "genre", type = Join.Type.LEFT_FETCH)
 	List<Genre> findAll();
 
 	@Override
 	@NonNull
-//	@Join(value = "genre", type = Join.Type.LEFT_FETCH)
 	Optional<Genre> findById(Long id);
 
 }
